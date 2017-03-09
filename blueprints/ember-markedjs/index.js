43f7fcf1fece0f877dbc0b23b5c90d88ec5a42ba
David Pett <dpett@folklore.digital>
2017-03-08 19:38:10 -0600

    added packages

--- blueprints/ember-markedjs/index.js
+++ blueprints/ember-markedjs/index.js
@@ -5,6 +5,11 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonToProject('ember-browserify');
+    return this.addAddonToProject('ember-browserify').then(function() {
+      this.addPackagesToProject([
+        { name: 'highlight.js', target: '^9.9.0' },
+        { name: 'marked', target: '^0.3.6' }
+      ]);
+    });
   }
 };
